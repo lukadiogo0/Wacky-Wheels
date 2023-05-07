93bea6907d19ff3ae2ca0538117fac1c87420d5d
lukadiogo0 <diogo_araujo_2003@hotmail.com>
2023-05-08 00:49:46 +0100

    New button animation
    New button hover, keyboard and controller on menus

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
 
     public GameObject pauseMenuUI;
+
+    public GameObject pauseFirstButton;
 
     //called once per frame
     void Update()
@@ -40,6 +43,11 @@ public class PauseMenu : MonoBehaviour
         //freeze gametime
         Time.timeScale = 0f;
         GameIsPaused = true;
+
+        //clear selected object
+        EventSystem.current.SetSelectedGameObject(null);
+        //set a new slected object
+        EventSystem.current.SetSelectedGameObject(pauseFirstButton);
     }
 
     public void LoadMenu()
--- Assets/Scripts/UnlockTrack.cs
+++ Assets/Scripts/UnlockTrack.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class UnlockTrack : MonoBehaviour
 {
     public GameObject Level2;
     public GameObject Level3;
     public GameObject Level4;
+
+    public GameObject level2Unlock, level3Unlock, level4Unlock;
+
+
 
     
 
@@ -17,7 +22,7 @@ public class UnlockTrack : MonoBehaviour
         //inserir condicao que torna interactable
         Level2.GetComponent<Button>().interactable = true; 
         Level3.GetComponent<Button>().interactable = true; 
-        //Level4.GetComponent<Button>().interactable = true; 
+        Level4.GetComponent<Button>().interactable = true; 
 
 
     }
@@ -33,5 +38,18 @@ public class UnlockTrack : MonoBehaviour
     public void Level4Unlock()
     {
         Level4.SetActive(false);
+    }
+
+    public void SelectLevel2(){
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(level2Unlock);
+    }
+    public void SelectLevel3(){
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(level3Unlock);
+    }
+    public void SelectLevel4(){
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(level4Unlock);
     }
 }
