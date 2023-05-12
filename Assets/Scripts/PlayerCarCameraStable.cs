5d28f1b17ac18ccc3f83e530ceae79228c56df13
Pedro Leiras <pedromdleiras@gmail.com>
2023-05-12 12:23:49 +0100

    Update car controller

--- Assets/Scripts/PlayerCarCameraStable.cs
+++ Assets/Scripts/PlayerCarCameraStable.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerCarCameraStable : MonoBehaviour
 {
 
-    public GameObject PlayerCar;
+    /*public GameObject PlayerCar;
     public float PlayerCarX;
     public float PlayerCarY;
     public float PlayerCarZ;
@@ -17,5 +17,36 @@ public class PlayerCarCameraStable : MonoBehaviour
         PlayerCarZ = PlayerCar.transform.eulerAngles.z;
 
         transform.eulerAngles = new Vector3(PlayerCarX - PlayerCarX, PlayerCarY, PlayerCarZ - PlayerCarZ);
+    }*/
+
+    public Vector3 offset;
+    public Transform player;
+
+    private KartController kartController;
+
+    public Vector3 origCamPos;
+    public Vector3 boostCamPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        kartController = player.GetComponent<KartController>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.position + offset;
+        transform.rotation = Quaternion.Slerp(transform.rotation, player.rotation, 3 * Time.deltaTime); //normal
+
+
+        if (kartController.BoostTime > 0)
+        {
+            transform.GetChild(0).localPosition = Vector3.Lerp(transform.GetChild(0).localPosition, boostCamPos, 3 * Time.deltaTime);
+        }
+        else
+        {
+            transform.GetChild(0).localPosition = Vector3.Lerp(transform.GetChild(0).localPosition, origCamPos, 3 * Time.deltaTime);
+        }
     }
 }
