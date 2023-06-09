5c5c3d021f1794e78c6bc7472ec42a4f42e228a4
JNun0 <joao.nuno669@outlook.pt>
2023-06-09 17:23:38 +0100

    Update

--- Assets/Scripts/CheckPoints/TrackCheckpoints.cs
+++ Assets/Scripts/CheckPoints/TrackCheckpoints.cs
@@ -3,18 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrackCheckpoints : MonoBehaviour {
-
-    [SerializeField] private List<Transform> carTransformList;
-
+public class TrackCheckpoints : MonoBehaviour
+{
+    [SerializeField] private Transform playerCarTransform;
     private List<CheckpointSingle> checkpointSingleList;
-    private List<int> nextCheckpointSingleIndexList;
+    private int nextCheckpointSingleIndex;
+    private bool isCooldown;
 
-    private void Awake() {
+    private void Awake()
+    {
         Transform checkpointsTransform = transform.Find("Checkpoints");
 
         checkpointSingleList = new List<CheckpointSingle>();
-        foreach (Transform checkpointSingleTransform in checkpointsTransform) {
+        foreach (Transform checkpointSingleTransform in checkpointsTransform)
+        {
             CheckpointSingle checkpointSingle = checkpointSingleTransform.GetComponent<CheckpointSingle>();
 
             checkpointSingle.SetTrackCheckpoints(this);
@@ -22,25 +24,59 @@ public class TrackCheckpoints : MonoBehaviour {
             checkpointSingleList.Add(checkpointSingle);
         }
 
-        nextCheckpointSingleIndexList = new List<int>();
-        foreach (Transform carTransform in carTransformList) {
-            nextCheckpointSingleIndexList.Add(0);
+        nextCheckpointSingleIndex = 0;
+        isCooldown = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && !isCooldown)
+        {
+            int previousCheckpointIndex = (nextCheckpointSingleIndex - 1 + checkpointSingleList.Count) % checkpointSingleList.Count;
+            int currentCheckpointIndex = (nextCheckpointSingleIndex + checkpointSingleList.Count) % checkpointSingleList.Count;
+
+            if (currentCheckpointIndex != previousCheckpointIndex)
+            {
+                StartCoroutine(StartCooldown());
+
+                Transform previousCheckpointTransform = checkpointSingleList[previousCheckpointIndex].transform;
+                Vector3 spawnPosition = previousCheckpointTransform.position;
+                Quaternion spawnRotation = Quaternion.LookRotation(-previousCheckpointTransform.right); // Look in the opposite direction along the X-axis
+
+                playerCarTransform.position = spawnPosition;
+                playerCarTransform.rotation = spawnRotation;
+
+                nextCheckpointSingleIndex = previousCheckpointIndex;
+
+                Debug.Log("Passed Checkpoint: " + checkpointSingleList[previousCheckpointIndex].name);
+            }
         }
     }
 
-    public void CarThroughCheckpoint(CheckpointSingle checkpointSingle, Transform carTransform) {
-        int nextCheckpointSingleIndex = nextCheckpointSingleIndexList[carTransformList.IndexOf(carTransform)];
-        if (checkpointSingleList.IndexOf(checkpointSingle) == nextCheckpointSingleIndex) {
-            // Correct checkpoint
-            Debug.Log("Correct");
-
-            nextCheckpointSingleIndexList[carTransformList.IndexOf(carTransform)]
-                = (nextCheckpointSingleIndex + 1) % checkpointSingleList.Count;
-        } else {
-            // Wrong checkpoint
-            Debug.Log("Wrong");
-        }
+    private IEnumerator StartCooldown()
+    {
+        isCooldown = true;
+        yield return new WaitForSeconds(1f); // Cooldown duration in seconds
+        isCooldown = false;
     }
 
+    public void CarThroughCheckpoint(CheckpointSingle checkpointSingle, Transform carTransform)
+    {
+        if (carTransform == playerCarTransform)
+        {
+            int nextCheckpointSingleIndex = checkpointSingleList.IndexOf(checkpointSingle);
+            if (nextCheckpointSingleIndex == this.nextCheckpointSingleIndex)
+            {
+                // Correct checkpoint
+                Debug.Log("Correct");
 
+                this.nextCheckpointSingleIndex = (nextCheckpointSingleIndex + 1) % checkpointSingleList.Count;
+            }
+            else
+            {
+                // Wrong checkpoint
+                Debug.Log("Wrong");
+            }
+        }
+    }
 }
