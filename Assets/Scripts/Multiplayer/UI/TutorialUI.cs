e6021b7610a417604e243c4e09b8cab63f91a974
Pedro Leiras <94070962+pedro-leiras@users.noreply.github.com>
2024-01-15 16:33:39 +0000

    update start countdown

--- Assets/Scripts/Multiplayer/UI/TutorialUI.cs
+++ Assets/Scripts/Multiplayer/UI/TutorialUI.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TutorialUI : MonoBehaviour
 {
+
+    [SerializeField] private TextMeshProUGUI keyInteractText;
     private void Start()
     {
+        GameInput.Instance.OnBindingRebind += GameInput_OnBindingRebind;
         WackyGameManager.Instance.OnLocalPlayerReadyChanged += KitchenGameManager_OnLocalPlayerReadyChanged;
+        UpdateVisual();
         Show();
     }
 
@@ -16,6 +21,16 @@ public class TutorialUI : MonoBehaviour
         {
             Hide();
         }
+    }
+
+    private void GameInput_OnBindingRebind(object sender, System.EventArgs e)
+    {
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        keyInteractText.text = "PRESS '" + GameInput.Instance.GetBindingText(GameInput.Binding.Interact) + "' TO START";
     }
 
     private void Show()
