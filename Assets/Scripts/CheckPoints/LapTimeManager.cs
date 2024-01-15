4f9bf063f7bf85c80d5a60f0f61fbc30e42ab94c
Pedro Leiras <94070962+pedro-leiras@users.noreply.github.com>
2024-01-15 16:50:43 +0000

    update time manager

--- Assets/Scripts/CheckPoints/LapTimeManager.cs
+++ Assets/Scripts/CheckPoints/LapTimeManager.cs
@@ -12,6 +12,7 @@ public class LapTimeManager : NetworkBehaviour
     private static int SecondCount = 0;
     private static float MiliSecondCount = 0;
     private static string MiliSecondDisplay;
+    private bool canUpdate = false;
 
     [SerializeField] private GameObject MinuteBox;
     [SerializeField] private GameObject SecondBox;
@@ -20,45 +21,51 @@ public class LapTimeManager : NetworkBehaviour
     void Update()
     {
         if (!IsOwner)  return;
+            if (canUpdate) { 
+            MinuteBox.SetActive(true);
+            SecondBox.SetActive(true);
+            MiliSecondBox.SetActive(true);
 
-        MinuteBox.SetActive(true);
-        SecondBox.SetActive(true);
-        MiliSecondBox.SetActive(true);
-
-        MiliSecondCount += Time.deltaTime * 10;
+            MiliSecondCount += Time.deltaTime * 10;
         
-        if (MiliSecondCount >= 10)
-        {
-            MiliSecondCount -= 10;
-            SecondCount += 1;
-        }
+            if (MiliSecondCount >= 10)
+            {
+                MiliSecondCount -= 10;
+                SecondCount += 1;
+            }
 
-        MiliSecondDisplay = Mathf.Floor(MiliSecondCount).ToString("F0");
-        MiliSecondBox.GetComponent<TextMeshProUGUI>().text = "" + MiliSecondDisplay;
+            MiliSecondDisplay = Mathf.Floor(MiliSecondCount).ToString("F0");
+            MiliSecondBox.GetComponent<TextMeshProUGUI>().text = "" + MiliSecondDisplay;
 
-        if (SecondCount <= 9)
-        {
-            SecondBox.GetComponent<TextMeshProUGUI>().text = "0" + SecondCount + ".";
-        }
-        else
-        {
-            SecondBox.GetComponent<TextMeshProUGUI>().text = "" + SecondCount + ".";
-        }
+            if (SecondCount <= 9)
+            {
+                SecondBox.GetComponent<TextMeshProUGUI>().text = "0" + SecondCount + ".";
+            }
+            else
+            {
+                SecondBox.GetComponent<TextMeshProUGUI>().text = "" + SecondCount + ".";
+            }
 
-        if (SecondCount >= 60)
-        {
-            SecondCount = 0;
-            MinuteCount += 1;
-        }
+            if (SecondCount >= 60)
+            {
+                SecondCount = 0;
+                MinuteCount += 1;
+            }
 
-        if (MinuteCount <= 9)
-        {
-            MinuteBox.GetComponent<TextMeshProUGUI>().text = "0" + MinuteCount + ":";
+            if (MinuteCount <= 9)
+            {
+                MinuteBox.GetComponent<TextMeshProUGUI>().text = "0" + MinuteCount + ":";
+            }
+            else
+            {
+                MinuteBox.GetComponent<TextMeshProUGUI>().text = "" + MinuteCount + ":";
+            }
         }
-        else
-        {
-            MinuteBox.GetComponent<TextMeshProUGUI>().text = "" + MinuteCount + ":";
-        }
+    }
+
+    public void SetCanUpdate(bool canUpdate)
+    {
+        this.canUpdate = canUpdate;
     }
 
     public void ResetTime()
